f4f29db8716d3e1b5238d9041e6a29dd773df73e
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-02-01 23:23:11 -0300

    chore: loader model in database

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Mendonça',
+    email: 'lsm5@cin.ufpe.br',
+    password_hash: '123456',
+  });
+  return res.json(user);
 });
 
 export default routes;
