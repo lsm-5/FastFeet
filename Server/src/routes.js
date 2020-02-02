1474a8cc31bc3d4d7f5ec0ecf3caf8de65799fe1
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-02-01 23:27:38 -0300

    feat: create route store of user

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/user';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Lucas Mendonça',
-    email: 'lsm5@cin.ufpe.br',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
