1dc1b1d4676274a51c297bc8ec5af614c5eaca67
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-02-04 20:48:26 -0300

    chore: validation notification update

--- Server/src/app/controllers/NotificationController.js
+++ Server/src/app/controllers/NotificationController.js
@@ -27,17 +27,35 @@ class NotificationController {
       .sort({ createdAt: 'desc' })
       .limit(20);
 
-    return res.json(notifications);
+    return res.status(200).json(notifications);
   }
 
   async update(req, res) {
+    const { email } = req.query;
+
+    if (!email) {
+      return res
+        .status(401)
+        .json({ error: 'email required to load notifications' });
+    }
+
+    const checkIsDelivery = await Deliverymen.findOne({
+      where: { id: req.userId, email },
+    });
+
+    if (!checkIsDelivery) {
+      return res
+        .status(401)
+        .json({ error: 'Only deliverymen can load notifications' });
+    }
+
     const notification = await Notification.findByIdAndUpdate(
       req.params.id,
       { read: true },
-      { new: true }
+      { new: true } // retorna a notification atualizada
     );
 
-    return res.json(notification);
+    return res.status(200).json(notification);
   }
 }
 
